d93522a399ecea71569176b0f50ee46967d782b9
Emiliano Spada <emilianos13@gmail.com>
2024-11-02 17:42:47 +0100

    refactoring nodbit

--- homeassistant/components/nodbit/auth.py
+++ homeassistant/components/nodbit/auth.py
@@ -10,141 +10,18 @@ to ensure valid authentication during API calls. Key functionalities include:
 - `retry_request`: Provides a retry mechanism with exponential backoff for network stability during authentication operations.
 """
 
-from http import HTTPStatus
 import json
 import logging
-import time
 
 import requests
 
-from .const import (
-    AUTH_DOMAIN,
-    HEADERS,
-    HTTP_TIMEOUT,
-    ID,
-    IDTOKEN_LIFETIME,
-    INITIAL_WAIT_TIME,
-    MAX_RETRIES,
-    REFRESHTOKEN_LIFETIME,
-)
+from .const import AUTH_DOMAIN, HEADERS, HTTP_TIMEOUT, ID
 
 _LOGGER = logging.getLogger(__name__)
 
-# Token cache
-id_token_cache = None
-id_token_expiry_time = None
-refresh_token_cache = None
-refresh_token_expiry_time = None
 
-
-def refresh_id_token() -> None:
-    """Use the Refresh Token to obtain a new ID Token.
-
-    Update cache with exponential retry.
-    """
-    global id_token_cache, id_token_expiry_time
-
-    _LOGGER.info("Refreshing ID Token")
-    refresh_payload = {
-        "AuthFlow": "REFRESH_TOKEN_AUTH",
-        "ClientId": ID,
-        "AuthParameters": {
-            "REFRESH_TOKEN": refresh_token_cache,
-            "SECRET_HASH": CONF_KEY,
-        },
-    }
-
-    wait_time = INITIAL_WAIT_TIME
-    retries = 0
-    while retries < MAX_RETRIES:
-        response = requests.post(
-            AUTH_DOMAIN,
-            headers=HEADERS,
-            json=refresh_payload,
-            timeout=HTTP_TIMEOUT,
-        )
-        obj = json.loads(response.text)
-
-        response_code = obj.get("statusCode")
-
-        if response_code == HTTPStatus.OK:
-            refresh_result = response.json()["AuthenticationResult"]
-            id_token_cache = refresh_result["IdToken"]
-            id_token_expiry_time = (
-                time.time() + IDTOKEN_LIFETIME
-            )  # New ID Token expiry in 1 hour
-
-            _LOGGER.info("ID Token refreshed successfully. Cache updated")
-            return
-
-        _LOGGER.info("Refresh attempt %d failed", retries + 1)
-        retries += 1
-        if retries < MAX_RETRIES:
-            _LOGGER.info("Retrying in %d seconds", wait_time)
-            time.sleep(wait_time)
-            wait_time *= 2
-        else:
-            _LOGGER.error("Failed to refresh ID Token after all retries")
-            raise ConnectionError
-
-
-def login() -> None:
-    """Log into Cognito to retrieve new tokens and update the cache."""
-    global \
-        id_token_cache, \
-        id_token_expiry_time, \
-        refresh_token_cache, \
-        refresh_token_expiry_time
-
-    _LOGGER.info("Logging in")
-    login_payload = {
-        "AuthFlow": "USER_PASSWORD_AUTH",
-        "ClientId": ID,
-        "AuthParameters": {
-            "USERNAME": CONF_USER_ID,
-            "PASSWORD": CONF_USER_PWD,
-            "SECRET_HASH": CONF_KEY,
-        },
-    }
-
-    wait_time = INITIAL_WAIT_TIME
-    retries = 0
-    while retries < MAX_RETRIES:
-        response = requests.post(
-            AUTH_DOMAIN,
-            headers=HEADERS,
-            json=login_payload,
-            timeout=HTTP_TIMEOUT,
-        )
-
-        obj = json.loads(response.text)
-
-        response_code = obj.get("statusCode")
-
-        if response_code == HTTPStatus.OK:
-            auth_result = response.json()["AuthenticationResult"]
-            id_token_cache = auth_result["IdToken"]
-            refresh_token_cache = auth_result["RefreshToken"]
-            id_token_expiry_time = time.time() + IDTOKEN_LIFETIME
-            refresh_token_expiry_time = time.time() + REFRESHTOKEN_LIFETIME
-
-            _LOGGER.info("Logged in successfully. Tokens saved in cache")
-            return
-
-        _LOGGER.info("Refresh attempt %d failed", retries + 1)
-        retries += 1
-        if retries < MAX_RETRIES:
-            _LOGGER.info("Retrying in %d seconds", wait_time)
-            time.sleep(wait_time)
-            wait_time *= 2
-        else:
-            _LOGGER.error("Failed to login after all retries")
-            raise ConnectionError
-
-
-def login2(user_id: str, user_pass: str, secret_hash: str) -> None:
-    """Log into Cognito to retrieve new tokens and update the cache."""
-    id_token = ""
+def login(user_id: str, user_pass: str, secret_hash: str) -> str:
+    """Log into Cognito to retrieve new tokens."""
 
     _LOGGER.info("Logging in")
     login_payload = {
@@ -166,40 +43,14 @@ def login2(user_id: str, user_pass: str, secret_hash: str) -> None:
     response.raise_for_status()
     obj = json.loads(response.text)
 
-    id_token = obj["AuthenticationResult"]["IdToken"]
+    id_tok = obj["AuthenticationResult"]["IdToken"]
 
-    _LOGGER.info("Logged in successfully. Tokens saved in cache")
-    return id_token
+    _LOGGER.info("Successfully logged in")
+    return id_tok
 
 
 def get_id_token(usr_id: str, usr_pwd: str, scr_hash: str) -> str:
     """Retrieve a valid ID Token, refresh or re-authenticate if necessary."""
     _LOGGER.info("Retrieving ID token")
-    token = login2(usr_id, usr_pwd, scr_hash)
-    return token
-
-
-def get_id_token2() -> str:
-    """Retrieve a valid ID Token, refresh or re-authenticate if necessary."""
-    global \
-        id_token_cache, \
-        id_token_expiry_time, \
-        refresh_token_cache, \
-        refresh_token_expiry_time
-
-    _LOGGER.info("Retrieving ID token")
-    if not id_token_cache or not refresh_token_cache:
-        _LOGGER.info("No tokens in cache")
-        login()
-        return id_token_cache
-
-    current_time = time.time()
-    if current_time >= id_token_expiry_time - 600:
-        if current_time >= refresh_token_expiry_time:
-            _LOGGER.info("Both tokens expired. Re-authenticating")
-            login()
-        else:
-            _LOGGER.info("ID Token is about to expire. Refreshing using Refresh Token")
-            refresh_id_token()
-
-    return id_token_cache
+    id_token = login(usr_id, usr_pwd, scr_hash)
+    return id_token
